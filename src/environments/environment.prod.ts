12355861df122bf8a4fa51e52c968ed502edf9ce
Gary Timeless <60399535+GaryTimeless@users.noreply.github.com>
2023-05-13 09:10:14 +0200

    new firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyC-yKYGEoUYvGyPCXEnKiyxz4aUolhYOv0',
-    authDomain: 'plannery-erp.firebaseapp.com',
-    projectId: 'plannery-erp',
-    storageBucket: 'plannery-erp.appspot.com',
-    messagingSenderId: '280968211876',
-    appId: '1:280968211876:web:dcc52292890c921521a531',
+    apiKey: "AIzaSyCq-QbG695Eyo1vwkZ-D9Ij3cULt8oFIcI",
+  authDomain: "gruppe3hka.firebaseapp.com",
+  databaseURL: "https://gruppe3hka-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "gruppe3hka",
+  storageBucket: "gruppe3hka.appspot.com",
+  messagingSenderId: "874067249902",
+  appId: "1:874067249902:web:e7c9ab361dff03d52f01ff",
+  measurementId: "G-11W8Z916BB"
   },
 };

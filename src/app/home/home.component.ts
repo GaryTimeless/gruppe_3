f10d2b574ecc1d9ab2c6c1edd8b243fa42f561fe
Gary Timeless <gary.timeless@gmx.de>
2023-05-31 11:40:08 +0200

    unnötigen code entfernt

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ResultDataParseService } from '../_services/result-data-parse.service';
-import { HttpClient } from '@angular/common/http';
-import { InputDataParseService } from '../_services/input-data-parse.service';
 
 @Component({
   selector: 'app-home',
@@ -10,29 +7,7 @@ import { InputDataParseService } from '../_services/input-data-parse.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(
-    private inputDataParseService: InputDataParseService,
-    private http: HttpClient
-  ) {}
+  constructor() {}
 
   ngOnInit(): void {}
-
-  
-
-  parseInputToXML() {
-    this.http.get('/assets/xml/input.json').subscribe((res) => {
-      const xml = this.inputDataParseService.parseToXML(res);
-      // console.log(xml)
-      this.downloadURI('data:text/xml;base64,' + btoa(xml), 'res.xml');
-    });
-  }
-
-  downloadURI(uri: string, name: string) {
-    var link = document.createElement('a');
-    link.download = name;
-    link.href = uri;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
 }
